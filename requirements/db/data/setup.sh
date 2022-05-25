b2c63d2ae5687bd6670870b8d1c1442036f489cd
Haenecour Jerome <73299695+Guaribaatlas@users.noreply.github.com>
2022-05-25 13:40:08 +0200

    Add files via upload

---
+++ requirements/db/data/setup.sh
@@ -0,0 +1,30 @@
+if [ -d "/run/mysqld" ]; then
+	echo "mysqld already exists."
+	chown -R mysql:mysql /run/mysqld
+else
+	echo "creating mysqld..."
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+
+if [ -d /var/lib/mysql/mysql ]; then
+	echo "MySQL directory already exists."
+	chown -R mysql:mysql /var/lib/mysql/mysql
+else
+	echo "creating MySQL directory..."
+	chown -R mysql:mysql /var/lib/mysql/mysql
+	mysql_install_db --user=mysql --ldata=/var/lib/mysql
+	tfile=$(mktemp)
+	cat << EOF > $tfile
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+
+	mysqld -u mysql --bootstrap --skip-networking=0 < $tfile
+	rm -f $tfile
+
+fi
+	mysqld -u mysql --skip-networking=0
